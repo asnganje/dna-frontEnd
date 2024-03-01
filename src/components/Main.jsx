5066fbbb27324439d20a02c0bc178c1cae4b0b2c
asnganje <abd.nganje@gmail.com>
2024-03-01 07:30:28 +0300

    Handle new user in the welcome message

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
+import { useState } from "react";
 
 const Main = () => {
     const navigate = useNavigate()
+    const [candidate, setCandidate] = useState(localStorage.getItem('user'))
+
     const openLogin = () => {
         navigate('/login')
     }
@@ -12,15 +15,32 @@ const Main = () => {
         navigate('/signup')
     }
 
+    const logoutHandler = () => {
+        setCandidate(
+            localStorage.setItem('user', "")
+        )
+    }
+
+    const modificationHandler = () => {
+        navigate('/modify')
+    }
 
     return(
         <main className="mx-[5%]">
             <Header/>
             <section className="mx-[5%]">
-                <div className="flex gap-2 mt-5">
+                {candidate?
+                (<div className="flex justify-between mt-5">
+                    <div className="flex flex-col">
+                    <button className="bg-blue-500 text-white w-full p-0.5 h-[70%] rounded-md" onClick={modificationHandler}>Manager User</button>
+                    <p>Welcome {candidate}...</p>
+                    </div>
+                    <button className="bg-blue-500 text-white w-[10%] p-0.5 h-[70%] rounded-md" onClick={logoutHandler}>Logout</button>
+                    </div>)
+                :(<div className="flex gap-2 mt-5">
                 <button className="bg-blue-500 text-white w-[10%] p-0.5 h-[70%] rounded-md" onClick={openLogin}>Login</button> | 
                 <button className="bg-blue-500 text-white w-[10%] p-0.5 h-[70%] rounded-md" onClick={openSignUp}>SignUp</button>
-                </div>
+                </div>)}
             </section>
             <Footer />
             
