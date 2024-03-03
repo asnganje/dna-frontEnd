ca530a7b41563e4c313331629ed88977b1364beb
asnganje <abd.nganje@gmail.com>
2024-03-03 08:39:19 +0300

    update front end

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,14 @@ import Footer from "./Footer";
 import Header from "./Header";
 import { useState } from "react";
 import banner from '../assets/be4.jpg'
+import bot from '../assets/bot1.jpg'
+import { GrNext } from "react-icons/gr";
+import { GrPrevious } from "react-icons/gr";
+import g1 from '../assets/g1.jpg'
+import g2 from '../assets/g2.jpg'
+import g3 from '../assets/g3.jpg'
+import g4 from '../assets/g4.jpg'
+import g5 from '../assets/g5.jpg'
 
 const Main = () => {
     const navigate = useNavigate()
@@ -55,9 +63,48 @@ const Main = () => {
                     검사시작
                     </button>
                 </div>
-                <div className="mx-[25%] mt-7">
-                    <img src={banner} alt="banner" />
+                <div className="mx-[5%] mt-7">
+                    <img src={banner} alt="banner"  className="w-full"/>
                 </div>
+            </section>
+            <section className="mx-[5%]">
+                <div className="text-center">
+                    <p>
+                    사용후기
+                    </p>
+                </div>
+                <div className="sm:flex sm:flex-row flex-col">
+                    <img src={g1} alt="g1" className="ml-5" />
+                    <img src={g2} alt="g2" />
+                    <img src={g3} alt="g3" />
+                    <img src={g4} alt="g4" />
+                    <img src={g5} alt="g5" />
+                </div>
+                <div className="flex justify-center mt-5 gap-5">
+                    <GrPrevious className="cursor-pointer"/><GrNext className="cursor-pointer"/>
+                </div>
+            </section>
+            <section className="mx-[5%] my-[10%]">
+                <div className="flex flex-col justify-center items-center">
+                    <h4>
+                    학습유전자 타입은 총 54개의 타입이 있습니다.
+                    </h4>
+                    <h4>
+                    회원가입 후 무료로 회원님의 학습유전자 타입을 발견해 보세요!
+                    </h4>
+                    <div className="mt-5 flex gap-2">
+                        <button className="bg-blue-500 text-white ml-1 h-[70%] rounded-md tracking-tighter">
+                        전체 사용후기
+                        </button>
+                        <button className="bg-white text-blue-500 border border-blue-400 ml-1 h-[70%] rounded-md tracking-tighter">
+                        4가지 학습유전자
+                        </button>
+                    </div>
+                </div>
+
+            </section>
+            <section className="mx-[5%]">
+                <img src={bot} alt="bottom" className="w-full" />
             </section>
             <Footer />
             
